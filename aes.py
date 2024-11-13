4957079f85a2e24b0b2fb3de981c80dc98b2ea98
DiddiShivani <diddishivani94@gmail.com>
2024-11-13 05:37:53 +0530

    first commit

--- aes.py
+++ aes.py
@@ -18,3 +18,23 @@ decrypted = unpad(cipher_dec.decrypt(encrypted), AES.block_size)
 original_string = decrypted.decode('utf-8')
 print("Original string: " + original_string + " " + as_hex(decrypted))
 
+
+'''AES (Advanced Encryption Standard) is a symmetric encryption algorithm widely used for secure data encryption.
+AES is both highly secure and efficient, making it suitable for a wide range of applications, including internet communications, financial data protection, and secure data storage.
+
+
+1. Symmetric Encryption: Both encryption and decryption use the same secret key.
+2. Block Cipher: AES operates on fixed-size blocks of data (128 bits).
+3. Variable Key Length: AES supports key sizes of 128, 192, or 256 bits, allowing flexibility based on security needs.
+4. Rounds: The algorithm involves multiple rounds of processing, with the number of rounds depending on the key size:
+    128-bit key: 10 rounds
+    192-bit key: 12 rounds
+    256-bit key: 14 rounds'''
+
+
+"""
+1. SubBytes: Each byte in the state matrix is substituted with a corresponding byte from a fixed substitution table (S-box), enhancing non-linearity.
+2. ShiftRows: Each row of the state matrix is shifted a certain number of positions to the left. This step ensures diffusion across rows.
+3. MixColumns: Each column of the state matrix is transformed by a matrix multiplication, adding further diffusion.
+4. AddRoundKey: The state matrix is XORed with the round key."""
+
